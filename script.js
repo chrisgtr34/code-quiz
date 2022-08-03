663d895260a4858108f6e639a36c43c2a413dd63
CHRIS <chris@CHRISs-MBP.attlocal.net>
2022-08-02 21:41:59 -0700

    update js and html

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ var nextBtn = document.getElementById('nextBtn');
 var questionBoxElement = document.getElementById('question-box');
 var questionElement = document.getElementById('question');
 var answerButton = document.getElementById('answerBtn');
+var choice1 = document.getElementById('1');
+var choice2 = document.getElementById('2');
+var choice3 = document.getElementById('3');
+var choice4= document.getElementById('4');
+var result = document.getElementById('score')
 var score = 0;
 var timeRemaining = 60;
 var timer;
@@ -11,44 +16,42 @@ var timer;
 var questions = [
     {
         question: 'What tag is used to define an image or add an image to an HTML page?',
-        answers: [
-            { text: '<div>', correct: false},
-            { text: '<table>', correct: false},
-            { text: '<img>', correct: true},
-            { text: '<meta>', correct: false}
-        ]
+        choice1: "<div>",
+        choice2: "<table>",
+        choice3: "<img>", 
+        choice4: "<meta>", 
+        correct: "3"
     },
     {
         question: 'What tag can be used to insert a line break or blank line in an HTML document?',
-        answers: [
-            {text: '<br></br>', correct: true},
-            { text: '<head></head>', correct: false},
-            { text: '<body></body>', correct: false},
-            { text: '<title></title>', correct: false}
-        ]
+        choice1: "<br></br>", 
+        choice2: "<head></head>",
+        choice3: "<body></body>",
+        choice4: "<title></title>", 
+        correct: "1"
     },
     {
         question: 'What tag is required in all HTML documents, and is used to define the title?',
-        answers: [
-            {text: '<head></head>', correct: false},
-            {text: '<body></body>', correct: false},
-            {text: '<title><title>', correct: true},
-            {text: '<br></br>', correct: false}
-        ]
+        choice1: "<head></head>",
+        choice2: "<body></body>",
+        choice3: "<title><title>", 
+        choice4: "<br></br>", 
+        correct: "3"
+        
     },
     {
         question: 'What declaration MUST be included as the first item in an HTML document before the tag and is used to provide instructions to the web browser?',
-        answers: [
-            {text: '<embed>', correct: false},
-            {text: '<caption>', correct: false},
-            {text: '<code>', correct: false},
-            {text: '<!DOCTYPE>', correct: true}
-        ]
+        choice1: "<embed>",
+        choice2: "<caption>",
+        choice3: "<code>",
+        choice4: "<!DOCTYPE>", 
+        correct: "4"
+        
     },
 ]
 
 let shuffleQuestions;
-let questionIndex;
+let questionIndex = 0;
 
 startBtn.addEventListener('click',() =>{
     startTimer();
@@ -80,63 +83,39 @@ function startQuiz() {
     shuffleQuestions = questions.sort(() => Math.random() - .5)
     console.log(shuffleQuestions)
     questionIndex = 0
-    nextQuestion();
-}
-
-
-function displayQuestions(question) {
-    console.log(question)
-    questionElement.innerText = question.question
-    question.forEach(answer => {
-        var button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('btn')
-
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener('click', answerChoice)
-        answerButton.appendChild(button)
-    })
-}
-
-
-function nextQuestion(){
-    // resetState()
     displayQuestions(shuffleQuestions)
 }
 
-function answerChoice (e) {
-    var selectedButton = e.target
-    var correct = selectedButton.dataset.correct
-    setStatusClass(document.body, correct)
-    Array.from(answerButton.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct)
-    })
-  // If the selected answer does not have the value
-  // true in it's data-correct property, remove 5 seconds.
-    if(!correct){
-     count -= 10
-        document.getElementById('time').innerHTML=count;
-    }
-    if (shuffleQuestions.length > questionIndex + 1) {
-        nextBtn.classList.remove('hide')
-    } else {
-        startBtn.innerText = 'Restart'
-        startBtn.classList.remove('hide')
-    }
+
+function displayQuestions() {
+    var q = questions[questionIndex];
+    questionElement.innerHTML = q.question;
+    choice1.innerText = q.choice1;
+    choice2.innerText = q.choice2;
+    choice3.innerText = q.choice3;
+    choice4.innerText = q.choice4;
 }
 
-function setStatusClass(element, correct) {
-    clearStatusClass(element)
-    if (correct) {
-      element.classList.add('correct')
-    } else {
-      element.classList.add('wrong')
+function checkAnswer(answer) {
+    if(questions[questionIndex].correct == answer) {
+        answerChoice.textContent = "Correct!"
     }
-  }
+    else {
+       answerChoice.textContent = "Wrong!"
+       timeRemaining-=10;
+    }
 
-  function clearStatusClass(element) {
-    element.classList.remove('correct')
-    element.classList.remove('wrong')
-  }
+    if (questions.length === questionIndex+1) {
+        showResults(); // If it has gone through all questions, show final score
+        return;
+    }
+        questionIndex++;
+        displayQuestions();
+    };
+
+    function showResults() {
+        if (timeRemaining === 0 || questions.length -1) { 
+         result.textContent = "Your final score is " + timeRemaining + ".";
+        }
+     };
+     
